f269287b72bd12f85b1c1d3e8c0d666c4b24d861
Zas <henriquezerbini@gmail.com>
2018-11-09 15:09:11 -0200

    criacao da:conexao com bd; session; rotas. no app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,15 +1,20 @@
+/*-----------------------------------------------------------------
+                        Importa Módulos
+-----------------------------------------------------------------*/
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var expressSession = require('express-session');
+var sqliteStore = require('connect-sqlite3')(expressSession);
+var connection = require('express-myconnection');
+var mysql = require('mysql');
 
 var app = express();
-
-// view engine setup
+/*----------------------------------------------------------------
+                      Engine Setup
+----------------------------------------------------------------*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -18,17 +23,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+/*----------------------------------------------------------------
+                          ROTAS
+----------------------------------------------------------------*/
+app.use('/', './routes/index');
+app.use('/users', './routes/users');
+/*----------------------------------------------------------------
+                        CRIAR SESSION
+-----------------------------------------------------------------*/
+app.use(expressSession({
+  store: new sqliteStore,
+  secret: 'X87sZ34', // informa a chave criptografica da sua session
+  cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 1 semana
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+/*----------------------------------------------------------------
+                    Criar conexao com o BD
+----------------------------------------------------------------*/
+app.use(
+  connection(mysql, {
+    host: 'sql10.freemysqlhosting.net', // servidor do banco mysql, se for local: localhost
+    user: 'sql110263389', //usuario com permissao de conexao a BD
+    password: 'wRimM2EhX3', // senha de acesso ao BD
+    port: 3306,// porta do mysql, normalmente 3306
+    database: 'sql10263389'//nome da base de dados (esquema)
+  }, 'pool')
+);
+/*--------------------------------------------------------------*/
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +65,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+/*---------------------------------------------------------------*/
 module.exports = app;
