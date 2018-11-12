813e9b225574d4c52f2cd0e4be323aee3f0590fb
Zas <henriquezerbini@gmail.com>
2018-11-12 10:21:54 -0200

    inserido alguns comandos console.log

--- myapp/routes/usuario.js
+++ myapp/routes/usuario.js
@@ -6,11 +6,16 @@ router.post('/cadastraUsuario', function (req, res, next) {
   console.log(input);
   req.getConnection(function (err, connection) {
     var query = connection.query("INSERT INTO usuario SET ?", input, function (err, rows) {
+
       if (err) {
-        res.json({   status: 'ERRO', data: +err });
-        console.log("Erro ao cadastrar o usuario!!!!")
+        res.json({ status: 'ERRO', data: +err });
+        console.log("Erro ao cadastrar o usuario!!!!");
       }
-      else res.json({ status: 'OK', data: 'Incluido com sucesso!' });
+      else {
+        res.json({ status: 'OK', data: 'Incluido com sucesso!' });
+        console.log("Usuario inserido com sucesso!!!");
+      }
+      
     })
   });
 });
