418056e31b6d7d2d865abeac33c88e8b75c9d9e2
Zas <henriquezerbini@gmail.com>
2018-11-13 20:19:40 -0200

    Adicionado incompleto o sistema de cadastro de cachorros.

---
+++ myapp/public/javascripts/cadastroAnimalJS.js
@@ -0,0 +1,33 @@
+function cadastraAnimal() {
+    var form = document.formSignup;
+    var input = {
+        usuario_idUsuario: 4, // aprender como recuperar
+        nome: form.nomeAnimal.value,
+        idade: form.idadeAnimal.value,
+        tipo: form.tipoAnimal.value,
+        raca: form.racaAnimal.value,
+        sexo: form.sexoAnimal.value,
+        cor: form.corAnimal.value,
+        situacao: form.situacaoAnimal.value
+    };
+    console.log(input);
+    $.ajax({
+        url: '/animal/cadastroAnimal',
+        type: 'post',
+        data: input,
+        error: function (dados) {
+            alert('Erro1: ' + dados.data);
+        },
+        success: function (dados) {
+            if (dados.status === 'ERRO') {
+                alert('Erro2: ' + dados.data);
+                $("#alertaLogin").addClass("show");
+                $("#alertaLogin").removeClass("close");
+            }
+            else {
+                alert("Cadastro foi feito com sucesso!!!!");
+                window.location.href = '/index.html';
+            }
+        }
+    });
+}
--- myapp/routes/animal.js
+++ myapp/routes/animal.js
@@ -1,4 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/cadastroAnimal', function (req, res, next) {
+    var input = req.body;
+    console.log(input);
+    req.getConnection(function (err, connection) {
+        var query = connection.query("INSERT INTO animal SET ?", input, function (err, rows) {
+
+            if (err) {
+                res.json({ status: 'ERRO', data: +err });
+                console.log("Erro ao cadastrar o animal!!!!");
+            }
+            else {
+
+                res.json({ status: 'OK', data: 'Incluido com sucesso!' });
+                console.log("Animal inserido com sucesso!!!");
+            }
+
+        })
+    });
+});
+
+
 module.exports = router;
