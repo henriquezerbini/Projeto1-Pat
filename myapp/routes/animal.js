f543f7ab8317a8c9b014423db05815fc915729c6
Zas <henriquezerbini@gmail.com>
2018-11-14 13:23:56 -0200

    Implementamos o cadastro de cachorro para cada usuario logado!

--- myapp/routes/animal.js
+++ myapp/routes/animal.js
@@ -6,19 +6,30 @@ router.post('/cadastroAnimal', function (req, res, next) {
         var input = req.body;
         console.log(input);
         req.getConnection(function (err, connection) {
-            var query = connection.query("INSERT INTO animal SET ?", input, function (err, rows) {
-
-                if (err) {
-                    res.json({ status: 'ERRO', data: +err });
-                    console.log("Erro ao cadastrar o animal!!!!");
+            var query = connection.query("SELECT idUsuario FROM usuario WHERE user = ? " , req.session.login , function(err,rows){
+                input.usuario_idUsuario = rows[0].idUsuario;
+                console.log(input);
+                if(err)
+                {
+                    res.json({ status:'ERRO' , data: +err});
+                    console.log("Erro ao cadastrar o Animal");
                 }
-                else {
+                else{
+                    var query = connection.query("INSERT INTO animal SET ?", input, function (err2, rows) {
 
-                    res.json({ status: 'OK', data: 'Incluido com sucesso!' });
-                    console.log("Animal inserido com sucesso!!!");
+                        if (err) {
+                            res.json({ status: 'ERRO', data: +err2 });
+                            console.log("Erro ao cadastrar o animal!!!!");
+                        }
+                        else {
+        
+                            res.json({ status: 'OK', data: 'Incluido com sucesso!' });
+                            console.log("Animal inserido com sucesso!!!");
+                        }
+        
+                    });
                 }
-
-            })
+            });
         });
     }
     else{
